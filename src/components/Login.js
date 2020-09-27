0763fd1740583607e609bf4b3ca7eca4424f9605
mahavonjy <mahavonjy.cynthion@gmail.com>
2020-09-27 14:03:06 +0200

    add image and edit style

--- src/components/Login.js
+++ src/components/Login.js
@@ -20,6 +20,7 @@ class Login extends Component {
         this.state = {
             user: '',
             pass: '',
+            secure_pass: true,
             loader: false,
         }
     }
@@ -91,30 +92,47 @@ class Login extends Component {
 
                 <Image
                     style={styles.stretch}
-                    source={{uri: 'https://zupimages.net/up/19/18/3ltf.png'}}
+                    source={require('../images/ISL_logo.png')}
                 />
 
                 <View style={styles.islTextPosition}>
                     <Text style={styles.islText}>CREATIVE TICKET</Text>
                 </View>
 
-                <TextInput
-                    style={styles.input}
-                    placeholder="Utilisateur"
-                    autoCapitalize='none'
-                    onChangeText={user => this.setState({user})}
-                    value={user}
-                />
+                <View style={styles.SectionStyle}>
+                    <TextInput
+                        style={styles.input}
+                        placeholder="Utilisateur"
+                        autoCapitalize='none'
+                        onChangeText={user => this.setState({user})}
+                        value={user}
+                    />
+                    <Image source={require('../images/user.png')}
+                           style={styles.iconUser}/>
+                </View>
 
-                <TextInput
-                    style={styles.input}
-                    placeholder="Mot de passe"
-                    autoCapitalize='none'
-                    onChangeText={pass => this.setState({pass})}
-                    value={pass}
-                    secureTextEntry
-                    keyboardType="default"
-                />
+                <View style={styles.SectionStyle}>
+                    <TextInput
+                        style={styles.input}
+                        placeholder="Mot de passe"
+                        autoCapitalize='none'
+                        onChangeText={pass => this.setState({pass})}
+                        value={pass}
+                        secureTextEntry={this.state.secure_pass}
+                        keyboardType="default"
+                    />
+                    {this.state.secure_pass ?
+                        <TouchableOpacity onPress={() => this.setState({secure_pass: false})}>
+                            <Image
+                                source={require('../images/blind-symbol-of-an-opened-eye-with-a-slash.png')}
+                                style={styles.IconStyle}/>
+                        </TouchableOpacity> :
+                        <TouchableOpacity onPress={() => this.setState({secure_pass: true})}>
+                            <Image
+                                source={require('../images/eye.png')}
+                                style={styles.IconStyle}/>
+                        </TouchableOpacity>}
+                </View>
 
                 <TouchableOpacity style={styles.btn} onPress={this._onLogin.bind(this)}>
                     {this.state.loader ? <ActivityIndicator size="small" color="#00ff00"/> :
@@ -135,18 +153,34 @@ const styles = StyleSheet.create({
         justifyContent: 'center',
         alignItems: 'center',
         padding: 5,
-
-//        backgroundColor: '#c0d6f1'
+    },
+    SectionStyle: {
+        flexDirection: 'row',
+        backgroundColor: '#fff',
+        height: 40,
+        borderRadius: 5,
+        width: 250,
+        marginBottom: 15,
+    },
+    IconStyle: {
+        padding: 10,
+        margin: 8,
+        height: 25,
+        width: 25,
+        resizeMode: 'stretch',
+        alignItems: 'center'
+    },
+    iconUser: {
+        padding: 10,
+        margin: 8,
+        height: 23,
+        width: 23,
+        resizeMode: 'stretch',
+        alignItems: 'center',
     },
     input: {
-        height: 40,
-        width: 250,
-        paddingLeft: 10,
-        paddingRight: 10,
-        borderRadius: 5,
-        marginBottom: 15,
-        backgroundColor: '#fff',
-        color: '#000000'
+        flex: 1,
+        paddingLeft: 10
     },
     btn: {
         height: 40,
